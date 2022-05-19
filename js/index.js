dd0097dbecf20a1d612be5919f0c772c99639560
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-05-19 21:23:26 +0300

    Adding the audio variables to js and the playAudio(color) function

--- js/index.js
+++ js/index.js
@@ -10,30 +10,54 @@ var audio_red = document.getElementById("AudioRed");
 var audio_yellow = document.getElementById("AudioYellow"); 
 var audio_blue = document.getElementById("AudioBlue"); 
 
+function returnColor() { element.style.backgroundColor = current_color}
 
-function playGreen() {
-    audio_green.play();
-    // document.getElementById("green-button").style.backgroundColor = "white"
+// let test = document.getElementById("green-button").style.backgroundColor;
+// document.getElementById("text-area").innerHTML = test;
+
+// let test = document.getElementById("green-button");
+// let cssObj= window.getComputedStyle(test, null);
+// let bgColor = cssObj.getPropertyValue("background-color");
+// console.log(bgColor)
+
+function playAudio(color) {
+
+    // play audio
+    if (color === "green") {
+        audio_green.play();
+    }
+    else if (color === "red") {
+        audio_red.play();
+    }
+    else if (color ==="yellow") {
+        audio_yellow.play();
+    }
+    else if (color === "blue") {
+        audio_blue.play();
+    }
+
+
+
+    let element = document.getElementById(color);
+    let cssObj= window.getComputedStyle(element, null);
+    let bgColor = cssObj.getPropertyValue("background-color");
+
+    element.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    element.style.boxShadow =  "0 16px 32px 0 rgba(255, 255, 255, 0.8)";
+
+    setTimeout(function(){
+        element.style.backgroundColor = bgColor;
+        element.style.boxShadow = null;
+
+    } ,200)
 }
 
-function playRed() {
-    audio_red.play();
-}
-
-function playYellow() {
-    audio_yellow.play();
-}
-
-function playBlue() {
-    audio_blue.play();
-}
 
 
-
-document.getElementById("green-button").addEventListener("click", playGreen);
-document.getElementById("red-button").addEventListener("click", playRed);
-document.getElementById("yellow-button").addEventListener("click", playYellow);
-document.getElementById("blue-button").addEventListener("click", playBlue);
+document.getElementById("green").addEventListener("click", function(){playAudio('green')});
+document.getElementById("red").addEventListener("click", function(){playAudio('red')});
+document.getElementById("yellow").addEventListener("click", function(){playAudio('yellow')});
+document.getElementById("blue").addEventListener("click", function(){playAudio('blue')});
 
 
 
