0add7d25c0dbdaf93801f981bd004ff5ddeb422f
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-05-19 18:04:19 +0300

    Adding event listener to the button, writing play sounds functions

--- js/index.js
+++ js/index.js
@@ -1,8 +1,39 @@
-document.getElementById("greenID").addEventListener("click", myfunction);
+// document.getElementById("greenID").addEventListener("click", myfunction);
 
 
-function myfunction() {
-    console.log("helloooooooo");
+// function myfunction() {
+//     console.log("helloooooooo");
+// }
+
+var audio_green = document.getElementById("AudioGreen"); 
+var audio_red = document.getElementById("AudioRed"); 
+var audio_yellow = document.getElementById("AudioYellow"); 
+var audio_blue = document.getElementById("AudioBlue"); 
+
+
+function playGreen() {
+    audio_green.play();
+    // document.getElementById("green-button").style.backgroundColor = "white"
 }
 
-console.log("helloooooooo");
+function playRed() {
+    audio_red.play();
+}
+
+function playYellow() {
+    audio_yellow.play();
+}
+
+function playBlue() {
+    audio_blue.play();
+}
+
+
+
+document.getElementById("green-button").addEventListener("click", playGreen);
+document.getElementById("red-button").addEventListener("click", playRed);
+document.getElementById("yellow-button").addEventListener("click", playYellow);
+document.getElementById("blue-button").addEventListener("click", playBlue);
+
+
+
